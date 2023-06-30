01679cab72d980556b64e9b517acf06a5eb844d0
BrisaContreras872 <contrerasbrisa@micorreo.upp.edu.mx>
2023-06-29 19:48:19 -0600

    Initial commit

---
+++ app/src/main/java/com/example/boutiqueshop/RecuContra.java
@@ -0,0 +1,14 @@
+package com.example.boutiqueshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecuContra extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recu_contra);
+    }
+}
